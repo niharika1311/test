56a3a3859609b5a64dbf7d3bc6d98e8af375a4e4
niharika <niharika.pendem@gmail.com>
2018-03-11 17:06:04 +0530

    deletion

--- sll.cc
+++ sll.cc
@@ -25,6 +25,15 @@ class SLL : public Node {
   public:
   SLL():head{nullptr}, tail{nullptr} {
   }
+  int no_of_nodes() {
+    Node *temp = head;
+    int c = 1;
+    while(temp != tail) {
+      c++;
+      temp = temp->next;
+    }
+    return c;
+  }
   void insertNode(int d) {
     Node *temp = createNode(d);
     if(head == nullptr) 
@@ -42,19 +51,77 @@ class SLL : public Node {
     }
     cout << tail->data << "->nullptr" << endl;
   }
+  void insertFront(int data) {
+    Node *temp = createNode(data);
+    if(head == nullptr) 
+      head = tail = temp;
+    else {
+      temp->next = head;
+      head = temp;
+    }
+  }
+  void insertAtPos(int pos, int data) {
+    Node *node_pos = createNode(data);
+    int count = no_of_nodes();
+    if(pos == count)
+      insertNode(data);
+    else {
+      Node *temp = head;
+      for(int i = 1; i < pos; i++)
+        temp = temp->next;
+      node_pos->next = temp->next;
+      temp->next = node_pos;
+    }
+  }
+
+  void delete_node(int data) {
+    if(head->data == data) 
+      head = head->next;
+    else {          
+      Node *temp = head, *prev;
+      while(temp != nullptr && temp->data != data) {
+        prev = temp;
+        temp = temp->next;
+      }
+      prev->next = temp->next;
+      if(temp == tail)
+        tail = prev;
+    }
+  }
+
+  void delete_node_pos(int pos) {
+    Node *prev = head;
+    int count = no_of_nodes();
+    if(pos == 1)
+      head = head->next;
+    for(int i = 1; prev != nullptr && i < pos - 1; i++)
+      prev = prev->next;
+    prev->next = prev->next->next;
+    if(pos == count)
+      tail = prev;
+  }
 };
 
 int main() {
   SLL *obj = new SLL;
   obj->insertNode(10);
   obj->printList();
-  obj->insertNode(20);
+  obj->insertFront(20);
   obj->printList();
   obj->insertNode(30);
   obj->printList();
   obj->insertNode(40);
+  obj->insertFront(10);
   obj->printList();
   obj->insertNode(50);
+  obj->printList();
+  obj->insertAtPos(5,200);
+  obj->printList();
+  obj->delete_node(40);
+  obj->printList();
+  obj->delete_node_pos(6);
+  obj->printList();
+  obj->delete_node_pos(2);
   obj->printList();
   return 0;
 }
